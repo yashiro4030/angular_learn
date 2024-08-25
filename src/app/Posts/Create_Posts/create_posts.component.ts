ac6789a2dda0c18c63ae76994ee9d84d3128d783
unknown <admin@DCHARAI-SI.Um6ss.Root.local>
2024-08-25 01:25:44 +0100

    Version rec de input aff

--- src/app/Posts/Create_Posts/create_posts.component.ts
+++ src/app/Posts/Create_Posts/create_posts.component.ts
@@ -5,7 +5,8 @@ import { Component } from "@angular/core";
 @Component({
 
     selector:'app-CreatePosts',
-    templateUrl:'./create_posts.component.html'
+    templateUrl:'./create_posts.component.html',
+    styleUrl:'./create_posts.component.css'
 })
 export class CreatePostComponent{
 
